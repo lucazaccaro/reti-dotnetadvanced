0b0cde7cf762234261a7cc70d63b359c1eea8afa
Andrea Sanguin <andrea.sanguin@reti.it>
2022-03-04 16:53:16 +0100

    [Corso.NetAdvance2022] Exercises for EF Core 6 used with .Net 6

---
+++ NET6.EntityFramework/NET6.EF.ReverseEngineering/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NET6.EF.ReverseEngineering
+{
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Posts = new HashSet<Post>();
+        }
+
+        public int BlogId { get; set; }
+        public string Url { get; set; } = null!;
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
